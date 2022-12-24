1f69acf9c1dde217def9e308ac7da4a9badcc8fa
K-stiles <michealessuman08@gmail.com>
2022-12-24 10:08:57 +0000

    done

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://nervous-cow-stockings.cyclic.app",
-  // "http://127.0.0.1:5500", //TODO: Remove during production
-  // "http://localhost:3000", //TODO: Remove during production
+  "http://127.0.0.1:5500", //TODO: Remove during production
+  "http://localhost:3000", //TODO: Remove during production
 ];
 
 export default allowedOrigins;
